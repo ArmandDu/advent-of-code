4b8454c9060b77ccc874b59e7d8436e89031f7b4
Armand Dupuis <armand.dupuis@newthings.co>
2021-12-12 09:01:07 +0200

    Convert Day 3 to use impl Solution trait

--- 2021/src/day03.rs
+++ 2021/src/day03.rs
@@ -1,142 +1,137 @@
-pub fn part1() {
-    println!(
-        "day 3 part 1: v0({}) v1({}) v2({})",
-        v0::part1(),
-        v1::part1(),
-        v2::part1()
-    );
-}
-pub fn part2() {
-    println!(
-        "day 3 part 2: v0({}) v1({}) v2({})",
-        v0::part2(),
-        v1::part2(),
-        v2::part2()
-    );
-}
+use year2021::Solution;
 
-mod v2 {
-    use crate::day03::v1;
+pub mod v2 {
+    use year2021::Solution;
 
-    fn get_input() -> (usize, Vec<usize>) {
-        let input = include_str!("../data/day03_input");
-        let length = input.find("\n").unwrap();
+    pub struct Day03;
 
-        let pool = input
-            .lines()
-            .filter_map(|line| usize::from_str_radix(&line, 2).ok())
-            .collect();
+    impl Solution<usize, usize> for Day03 {
+        const DAY: u32 = 3;
+        const YEAR: u32 = 2021;
+        const TITLE: &'static str = "Binary Diagnostic (V2)";
+        type Input = (usize, Vec<usize>);
 
-        (length, pool)
+        fn part1(input: &Self::Input) -> Option<usize> {
+            let (length, content) = input.to_owned();
+
+            let gamma_rate: usize = (0..length)
+                .map(|shift| (Self::get_max_byte_at(shift, &content) as usize) << shift)
+                .sum();
+            let epsilon_rate = 2_usize.pow(length as u32) - 1 - gamma_rate;
+
+            Some(gamma_rate * epsilon_rate)
+        }
+
+        fn part2(input: &Self::Input) -> Option<usize> {
+            let (length, content) = input.to_owned();
+
+            let (oxygen, co2) = (0..length).rev().fold(
+                (content.to_vec(), content),
+                |(mut oxygen, mut co2), shift| {
+                    if oxygen.len() > 1 {
+                        let bit_oxygen = Self::get_max_byte_at(shift, &oxygen) as usize;
+
+                        oxygen.retain(|num| (num >> shift) & 1 == bit_oxygen);
+                    }
+
+                    if co2.len() > 1 {
+                        let bit_co2 = !Self::get_max_byte_at(shift, &co2) as usize;
+
+                        co2.retain(|num| (num >> shift) & 1 == bit_co2);
+                    }
+
+                    (oxygen, co2)
+                },
+            );
+
+            let oxygen = oxygen[0];
+            let co2 = co2[0];
+
+            Some(oxygen * co2)
+        }
+
+        fn parse(input: &str) -> Result<Self::Input, &str> {
+            let length = input.find("\n").unwrap();
+
+            let pool = input
+                .lines()
+                .filter_map(|line| usize::from_str_radix(&line, 2).ok())
+                .collect();
+
+            Ok((length, pool))
+        }
     }
 
-    fn get_max_byte_at(shift: usize, pool: &[usize]) -> bool {
-        pool.iter().map(|value| (value >> shift) & 1).sum::<usize>() * 2 >= pool.len()
-    }
-
-    pub fn part1() -> usize {
-        let (length, content) = get_input();
-
-        let gamma_rate: usize = (0..length)
-            .map(|shift| (get_max_byte_at(shift, &content) as usize) << shift)
-            .sum();
-        let epsilon_rate = 2_usize.pow(length as u32) - 1 - gamma_rate;
-
-        assert_eq!(gamma_rate * epsilon_rate, v1::part1());
-        gamma_rate * epsilon_rate
-    }
-
-    pub fn part2() -> usize {
-        let (length, content) = get_input();
-
-        let (oxygen, co2): (Vec<_>, Vec<_>) = (0..length).rev().fold(
-            (content.to_vec(), content),
-            |(mut oxygen, mut co2), shift| {
-                if oxygen.len() > 1 {
-                    let bit_oxygen = get_max_byte_at(shift, &oxygen) as usize;
-
-                    oxygen.retain(|num| (num >> shift) & 1 == bit_oxygen);
-                }
-
-                if co2.len() > 1 {
-                    let bit_co2 = !get_max_byte_at(shift, &co2) as usize;
-
-                    co2.retain(|num| (num >> shift) & 1 == bit_co2);
-                }
-
-                (oxygen, co2)
-            },
-        );
-
-        let oxygen = oxygen[0];
-        let co2 = co2[0];
-
-        assert_eq!(v1::part2(), oxygen * co2);
-        oxygen * co2
+    impl Day03 {
+        fn get_max_byte_at(shift: usize, pool: &[usize]) -> bool {
+            pool.iter().map(|value| (value >> shift) & 1).sum::<usize>() * 2 >= pool.len()
+        }
     }
 }
 
-mod v1 {
-    use crate::day03::v0;
+pub mod v1 {
+    use year2021::Solution;
 
-    pub fn part1() -> usize {
-        let v0_res = v0::part1();
-        let content: Vec<&str> = include_str!("../data/day03_input").lines().collect();
+    pub struct Day03;
 
-        let gamma_rate: String = (0..content.first().unwrap().len())
-            .map(|pos| {
-                content.iter().fold(String::new(), |mut byte, &row| {
-                    let bit: char = *row.chars().collect::<Vec<_>>().get(pos).unwrap();
+    impl Solution<usize, usize> for Day03 {
+        const DAY: u32 = 3;
+        const YEAR: u32 = 2021;
+        const TITLE: &'static str = "Binary Diagnostic (V1)";
+        type Input = Vec<String>;
 
-                    byte.push(bit);
-                    byte
+        fn part1(input: &Self::Input) -> Option<usize> {
+            let gamma_rate: String = (0..input.first().unwrap().len())
+                .map(|pos| {
+                    input.iter().fold(String::new(), |mut byte, row| {
+                        let bit: char = *row.chars().collect::<Vec<_>>().get(pos).unwrap();
+
+                        byte.push(bit);
+                        byte
+                    })
                 })
-            })
-            .map(|row| match row.chars().filter(|&bit| bit == '1').count() {
-                x if x > row.len() / 2 => '1',
-                _ => '0',
-            })
-            .collect();
+                .map(|row| match row.chars().filter(|&bit| bit == '1').count() {
+                    x if x > row.len() / 2 => '1',
+                    _ => '0',
+                })
+                .collect();
 
-        let epsilon_rate: String = gamma_rate
-            .chars()
-            .map(|b| match b {
-                '0' => '1',
-                _ => '0',
-            })
-            .collect();
+            let epsilon_rate: String = gamma_rate
+                .chars()
+                .map(|b| match b {
+                    '0' => '1',
+                    _ => '0',
+                })
+                .collect();
 
-        let gamma_rate = usize::from_str_radix(&gamma_rate, 2).unwrap();
-        let epsilon_rate = usize::from_str_radix(&epsilon_rate, 2).unwrap();
+            let gamma_rate = usize::from_str_radix(&gamma_rate, 2).unwrap();
+            let epsilon_rate = usize::from_str_radix(&epsilon_rate, 2).unwrap();
 
-        assert_eq!(gamma_rate * epsilon_rate, v0_res);
-        gamma_rate * epsilon_rate
-    }
+            Some(gamma_rate * epsilon_rate)
+        }
 
-    pub fn part2() -> usize {
-        let v0_res = v0::part2();
-        let content: Vec<String> = include_str!("../data/day03_input")
-            .lines()
-            .map(|line| String::from(line))
-            .collect();
-
-        let oxygen = find_recur(&content, 0, |ones, zeroes| {
-            match ones.len() >= zeroes.len() {
+        fn part2(input: &Self::Input) -> Option<usize> {
+            let oxygen = find_recur(&input, 0, |ones, zeroes| match ones.len() >= zeroes.len() {
                 true => ones,
                 _ => zeroes,
-            }
-        });
-        let co2 = find_recur(&content, 0, |ones, zeroes| {
-            match ones.len() < zeroes.len() {
+            });
+            let co2 = find_recur(&input, 0, |ones, zeroes| match ones.len() < zeroes.len() {
                 true => ones,
                 _ => zeroes,
-            }
-        });
-        let oxygen = usize::from_str_radix(&oxygen.unwrap(), 2).unwrap();
-        let co2 = usize::from_str_radix(&co2.unwrap(), 2).unwrap();
+            });
+            let oxygen = usize::from_str_radix(&oxygen.unwrap(), 2).unwrap();
+            let co2 = usize::from_str_radix(&co2.unwrap(), 2).unwrap();
 
-        assert_eq!(oxygen * co2, v0_res);
-        oxygen * co2
+            Some(oxygen * co2)
+        }
+
+        fn parse(input: &str) -> Result<Self::Input, &str> {
+            Ok(input
+                .trim()
+                .lines()
+                .map(|line| line.trim().to_string())
+                .collect())
+        }
     }
 
     fn find_recur<F>(pool: &Vec<String>, index: usize, mut candidate: F) -> Option<String>
@@ -158,19 +153,126 @@ mod v1 {
     }
 }
 
-/// INITIAL submission below
-mod v0 {
-    fn get_content() -> Vec<Vec<usize>> {
-        include_str!("../data/day03_input")
+pub mod v0_1 {
+    use crate::day03::Day03 as Day03V0;
+    use year2021::Solution;
+
+    pub struct Day03;
+
+    impl Solution<usize, usize> for Day03 {
+        const DAY: u32 = 3;
+        const YEAR: u32 = 2021;
+        const TITLE: &'static str = "Binary Diagnostic (V0.1)";
+        type Input = Vec<Vec<usize>>;
+
+        fn part1(input: &Self::Input) -> Option<usize> {
+            let flip_content: Vec<Vec<usize>> = (0..input.first().unwrap().len())
+                .map(|pos| {
+                    input.iter().fold(vec![], |mut acc, row| {
+                        acc.push(row.get(pos).unwrap().clone());
+
+                        acc
+                    })
+                })
+                .collect();
+
+            let gamma_rate: Vec<usize> = flip_content
+                .iter()
+                .map(|row| (row.iter().sum::<usize>() > row.len() / 2).into())
+                .collect();
+            let epsilon_rate: Vec<usize> = gamma_rate.iter().map(|b| 1 - b).collect();
+
+            Some(
+                Day03V0::byte_array_to_usize(&gamma_rate)
+                    * Day03V0::byte_array_to_usize(&epsilon_rate),
+            )
+        }
+
+        fn part2(_input: &Self::Input) -> Option<usize> {
+            None
+        }
+
+        fn parse(input: &str) -> Result<Self::Input, &str> {
+            Day03V0::parse(input)
+        }
+    }
+}
+
+/// INITIAL submission below (converted to impl Solution trait)
+pub struct Day03;
+
+impl Solution<usize, usize> for Day03 {
+    const DAY: u32 = 3;
+    const YEAR: u32 = 2021;
+    const TITLE: &'static str = "Binary Diagnostic (V0)";
+    type Input = Vec<Vec<usize>>;
+
+    fn part1(input: &Self::Input) -> Option<usize> {
+        let len = input.first().unwrap().len();
+        let mut gamma_rate: Vec<usize> = vec![];
+
+        //first attempt, imperative version
+        for i in 0..len {
+            let mut count1: usize = 0;
+
+            for j in 0..input.len() {
+                count1 += input.get(j).unwrap().get(i).unwrap();
+            }
+
+            if count1 > input.len() / 2 {
+                gamma_rate.push(1)
+            } else {
+                gamma_rate.push(0)
+            }
+        }
+
+        let epsilon_rate: Vec<usize> = gamma_rate.clone().iter().map(|b| 1 - b).collect();
+
+        let gamma_rate = Self::byte_array_to_usize(&gamma_rate);
+        let epsilon_rate = Self::byte_array_to_usize(&epsilon_rate);
+
+        Some(gamma_rate * epsilon_rate)
+    }
+
+    fn part2(input: &Self::Input) -> Option<usize> {
+        let oxygen = Self::find_recur(input.clone(), 0, |ones, zeroes| {
+            if ones.len() >= zeroes.len() {
+                ones
+            } else {
+                zeroes
+            }
+        })
+        .unwrap();
+        let co2 = Self::find_recur(input.clone(), 0, |ones, zeroes| {
+            if ones.len() < zeroes.len() {
+                ones
+            } else {
+                zeroes
+            }
+        })
+        .unwrap();
+
+        let oxygen = Self::byte_array_to_usize(&oxygen);
+        let co2 = Self::byte_array_to_usize(&co2);
+
+        Some(oxygen * co2)
+    }
+
+    fn parse(input: &str) -> Result<Self::Input, &str> {
+        Ok(input
+            .trim()
             .lines()
             .map(|line| {
-                line.split("")
+                line.trim()
+                    .split("")
                     .filter_map(|c| c.parse::<usize>().ok())
                     .collect()
             })
-            .collect()
+            .collect())
     }
+}
 
+impl Day03 {
     fn byte_array_to_usize(arr: &[usize]) -> usize {
         let (count, _) = arr
             .iter()
@@ -180,62 +282,11 @@ mod v0 {
         count
     }
 
-    pub fn part1_v2() -> usize {
-        let content = get_content();
-
-        let flip_content: Vec<Vec<usize>> = (0..content.first().unwrap().len())
-            .map(|pos| {
-                content.iter().fold(vec![], |mut acc, row| {
-                    acc.push(row.get(pos).unwrap().clone());
-
-                    acc
-                })
-            })
-            .collect();
-
-        let gamma_rate: Vec<usize> = flip_content
-            .iter()
-            .map(|row| (row.iter().sum::<usize>() > row.len() / 2).into())
-            .collect();
-        let epsilon_rate: Vec<usize> = gamma_rate.iter().map(|b| 1 - b).collect();
-
-        byte_array_to_usize(&gamma_rate) * byte_array_to_usize(&epsilon_rate)
-    }
-
-    pub fn part1() -> usize {
-        let content = get_content();
-        let len = content.first().unwrap().len();
-        let mut gamma_rate: Vec<usize> = vec![];
-
-        //first attempt, imperative version
-        for i in 0..len {
-            let mut count1: usize = 0;
-
-            for j in 0..content.len() {
-                count1 += content.get(j).unwrap().get(i).unwrap();
-            }
-
-            if count1 > content.len() / 2 {
-                gamma_rate.push(1)
-            } else {
-                gamma_rate.push(0)
-            }
-        }
-
-        let epsilon_rate: Vec<usize> = gamma_rate.clone().iter().map(|b| 1 - b).collect();
-
-        let gamma_rate = byte_array_to_usize(&gamma_rate);
-        let epsilon_rate = byte_array_to_usize(&epsilon_rate);
-
-        assert_eq!(gamma_rate * epsilon_rate, part1_v2());
-
-        gamma_rate * epsilon_rate
-    }
-
-    fn find_recur<F>(arr: Vec<Vec<usize>>, index: usize, mut candidate: F) -> Option<Vec<usize>>
-    where
-        F: FnMut(Vec<Vec<usize>>, Vec<Vec<usize>>) -> Vec<Vec<usize>>,
-    {
+    fn find_recur(
+        arr: Vec<Vec<usize>>,
+        index: usize,
+        candidate: impl Fn(Vec<Vec<usize>>, Vec<Vec<usize>>) -> Vec<Vec<usize>>,
+    ) -> Option<Vec<usize>> {
         return match arr.len() {
             0 => None,
             1 => Some(arr.first().unwrap().clone()),
@@ -255,34 +306,8 @@ mod v0 {
                             (ones, zeroes)
                         });
 
-                return find_recur(candidate(ones, zeroes), index + 1, candidate);
+                return Self::find_recur(candidate(ones, zeroes), index + 1, candidate);
             }
         };
-    }
-
-    pub fn part2() -> usize {
-        let content = get_content();
-
-        let oxygen = find_recur(content.clone(), 0, |ones, zeroes| {
-            if ones.len() >= zeroes.len() {
-                ones
-            } else {
-                zeroes
-            }
-        })
-        .unwrap();
-        let co2 = find_recur(content.clone(), 0, |ones, zeroes| {
-            if ones.len() < zeroes.len() {
-                ones
-            } else {
-                zeroes
-            }
-        })
-        .unwrap();
-
-        let oxygen = byte_array_to_usize(&oxygen);
-        let co2 = byte_array_to_usize(&co2);
-
-        oxygen * co2
     }
 }
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -13,6 +13,7 @@ fn main() {
         Some(7),
         Some(5),
     );
+
     day02::Day02::test(
         "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2",
         Some(150),
@@ -23,19 +24,44 @@ fn main() {
         Some(150),
         Some(900),
     );
+
+    day03::Day03::test(
+        "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010",
+        Some(198),
+        Some(230),
+    );
+    day03::v0_1::Day03::test(
+        "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010",
+        Some(198),
+        Some(230),
+    );
+    day03::v1::Day03::test(
+        "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010",
+        Some(198),
+        Some(230),
+    );
+    day03::v2::Day03::test(
+        "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010",
+        Some(198),
+        Some(230),
+    );
+
     day06::Day06V0::test("3,4,3,1,2", Some(5934), Some(26984457539));
     day06::Day06::test("3,4,3,1,2", Some(5934), Some(26984457539));
+
     day07::Day07::test("16,1,2,0,4,2,7,1,2,14", Some(37), Some(168));
 
     println!("\n---- SOLUTIONS ---\n");
-
-    day03::part1();
-    day03::part2();
 
     day01::Day01::run(include_str!("../data/day01_star1_input"));
 
     day02::Day02::run(include_str!("../data/day02_part1_input"));
     day02::Day02V2::run(include_str!("../data/day02_part1_input"));
+
+    day03::Day03::run(include_str!("../data/day03_input"));
+    day03::v0_1::Day03::run(include_str!("../data/day03_input"));
+    day03::v1::Day03::run(include_str!("../data/day03_input"));
+    day03::v2::Day03::run(include_str!("../data/day03_input"));
 
     day06::Day06V0::run(include_str!("../data/day06_input"));
     day06::Day06::run(include_str!("../data/day06_input"));
