67e11fe4ea9ff6f45afaa7cf12630c4721b1a2fc
Armand Dupuis <armand.dupuis@newthings.co>
2021-12-12 23:07:39 +0200

    Move from imperative to functional approach for Day09

--- 2021/src/day09.rs
+++ 2021/src/day09.rs
@@ -7,11 +7,10 @@ impl Solution<usize, usize> for Day09 {
     const DAY: u32 = 9;
     const YEAR: u32 = 2021;
     const TITLE: &'static str = "Smoke Basin";
-    type Input = (usize, usize, Vec<Vec<usize>>);
+    type Input = Vec<Vec<usize>>;
 
     fn part1(input: &Self::Input) -> Option<usize> {
-        let (_, _, h_map) = input;
-        let low_points = Self::find_low_points(&h_map);
+        let low_points = Self::find_low_points(&input);
 
         Some(
             low_points
@@ -23,15 +22,14 @@ impl Solution<usize, usize> for Day09 {
     }
 
     fn part2(input: &Self::Input) -> Option<usize> {
-        let (_, _, h_map) = input;
-        let low_points = Self::find_low_points(&h_map);
+        let low_points = Self::find_low_points(&input);
 
         let basins = low_points
             .iter()
             .map(|&(x, y, _)| {
                 let mut basin = vec![(x, y)];
 
-                Self::add_neighbors_to_basin(&mut basin, &h_map, x, y);
+                Self::add_neighbors_to_basin(&mut basin, &input, x, y);
 
                 basin
             })
@@ -44,11 +42,11 @@ impl Solution<usize, usize> for Day09 {
             .rev()
             .collect::<Vec<_>>();
 
-        (basin_sizes.len() >= 3).then(|| basin_sizes[0] * basin_sizes[1] * basin_sizes[2])
+        Some(basin_sizes.iter().take(3).product())
     }
 
     fn parse(input: &str) -> Result<Self::Input, &str> {
-        let h_map: Vec<_> = input
+        Ok(input
             .lines()
             .map(|line| {
                 line.trim()
@@ -56,89 +54,57 @@ impl Solution<usize, usize> for Day09 {
                     .filter_map(|d| d.parse::<_>().ok())
                     .collect()
             })
-            .collect();
-
-        let width = input.find("\n").expect("Invalid file");
-        let height = h_map.len();
-
-        Ok((width, height, h_map))
-    }
-}
-
-#[derive(Debug)]
-struct Pos {
-    top: Option<usize>,
-    left: Option<usize>,
-    bottom: Option<usize>,
-    right: Option<usize>,
-}
-
-impl Pos {
-    pub fn new(h_map: &Vec<Vec<usize>>, x: usize, y: usize) -> Self {
-        Self {
-            top: (y > 0).then(|| h_map[y - 1][x]),
-            left: (x > 0).then(|| h_map[y][x - 1]),
-            bottom: (y + 1 < h_map.len()).then(|| h_map[y + 1][x]),
-            right: (x + 1 < h_map[y].len()).then(|| h_map[y][x + 1]),
-        }
+            .collect())
     }
 }
 
 impl Day09 {
+    fn neighbors(width: usize, height: usize, x: usize, y: usize) -> Vec<(usize, usize)> {
+        [
+            (y > 0).then(|| (x, y - 1)),
+            (x > 0).then(|| (x - 1, y)),
+            (x + 1 < width).then(|| (x + 1, y)),
+            (y + 1 < height).then(|| (x, y + 1)),
+        ]
+        .iter()
+        .filter_map(|&c| c)
+        .collect()
+    }
+
     fn add_neighbors_to_basin(
         basin: &mut Vec<(usize, usize)>,
         h_map: &Vec<Vec<usize>>,
         x: usize,
         y: usize,
     ) {
-        let neighbors = Pos::new(&h_map, x, y);
+        let neighbors = Self::neighbors(h_map[0].len(), h_map.len(), x, y);
 
-        if matches!(neighbors.top, Some(val) if val < 9 && !basin.iter().any(|&(bx, by)| bx == x && by == y-1))
-        {
-            basin.push((x, y - 1));
+        neighbors
+            .into_iter()
+            .filter(|&(xi, yi)| h_map[yi][xi] < 9)
+            .for_each(|(xi, yi)| {
+                if !basin.iter().any(|&(bx, by)| bx == xi && by == yi) {
+                    basin.push((xi, yi));
 
-            Self::add_neighbors_to_basin(basin, &h_map, x, y - 1);
-        }
-        if matches!(neighbors.left, Some(val) if val < 9 && !basin.iter().any(|&(bx, by)| bx == x-1 && by == y))
-        {
-            basin.push((x - 1, y));
-
-            Self::add_neighbors_to_basin(basin, &h_map, x - 1, y);
-        }
-        if matches!(neighbors.bottom, Some(val) if val < 9 && !basin.iter().any(|&(bx, by)| bx == x && by == y+1))
-        {
-            basin.push((x, y + 1));
-
-            Self::add_neighbors_to_basin(basin, &h_map, x, y + 1);
-        }
-        if matches!(neighbors.right, Some(val) if val < 9 && !basin.iter().any(|&(bx, by)| bx == x+1 && by == y))
-        {
-            basin.push((x + 1, y));
-
-            Self::add_neighbors_to_basin(basin, &h_map, x + 1, y);
-        }
+                    Self::add_neighbors_to_basin(basin, &h_map, xi, yi);
+                }
+            });
     }
 
     fn find_low_points(h_map: &Vec<Vec<usize>>) -> Vec<(usize, usize, usize)> {
-        let mut low_points = vec![];
-        for y in 0..h_map.len() {
-            let row = &h_map[y];
+        let width = h_map[0].len();
+        let height = h_map.len();
 
-            for x in 0..row.len() {
-                let current = row[x];
+        (0..height)
+            .cartesian_product(0..width)
+            .fold(vec![], |mut low_points, (y, x)| {
+                let current = h_map[y][x];
+                let neighbors = Self::neighbors(width, height, x, y);
 
-                let neighbors = Pos::new(&h_map, x, y);
-                let is_higher_top = neighbors.top.unwrap_or(9) <= current;
-                let is_higher_left = neighbors.left.unwrap_or(9) <= current;
-                let is_higher_bottom = neighbors.bottom.unwrap_or(9) <= current;
-                let is_higher_right = neighbors.right.unwrap_or(9) <= current;
-
-                if !(is_higher_top || is_higher_left || is_higher_bottom || is_higher_right) {
+                if neighbors.iter().all(|&(xi, yi)| h_map[yi][xi] > current) {
                     low_points.push((x, y, current))
                 }
-            }
-        }
-
-        low_points
+                low_points
+            })
     }
 }
