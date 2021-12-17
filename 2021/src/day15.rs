74cdbf21f5269c09931c993b5018ef496e58413d
Armand Dupuis <armand.dupuis@newthings.co>
2021-12-17 08:12:26 +0200

    Refactor Day15 to increase speed
    Got it from 4.7 sec to 2.7 sec (in debug mode) but still quite slow :/
    
    Maybe A star's not the best algorithm?

--- 2021/src/day15.rs
+++ 2021/src/day15.rs
@@ -1,42 +1,49 @@
-use crate::utils::neighbors;
-use itertools::Itertools;
-use priority_queue::PriorityQueue;
-use std::collections::HashMap;
 use year2021::Solution;
+
+use crate::utils;
 
 pub struct Day15;
 
-impl Solution<usize, usize> for Day15 {
+impl Solution<i32, i32> for Day15 {
     const DAY: u32 = 15;
     const YEAR: u32 = 2021;
-    const TITLE: &'static str = "";
+    const TITLE: &'static str = "Chiton";
     type Input = (Vec<Vec<i32>>, usize, usize);
 
-    fn part1(input: &Self::Input) -> Option<usize> {
+    fn part1(input: &Self::Input) -> Option<i32> {
         let (map, width, height) = input;
 
-        Self::get_cost(map, width, height)
+        utils::a_star::solve(
+            &(0, 0),
+            &(*width - 1, *height - 1),
+            &(*width, *height),
+            |_, &(x, y)| Some(map[y][x]),
+            Self::manhattan_dist,
+        )
+        .and_then(|(_, cost)| Some(cost))
     }
 
-    fn part2(input: &Self::Input) -> Option<usize> {
+    fn part2(input: &Self::Input) -> Option<i32> {
         let (map, width, height) = input;
 
-        let actual_map: Vec<_> = (0..(5 * height))
-            .map(|y| {
-                (0..(5 * width))
-                    .map(|x| {
-                        let original = map[y % height][x % width];
+        let proj_width = *width * 5;
+        let proj_height = *height * 5;
 
-                        match original + (x / width + y / height) as i32 {
-                            x if x > 9 => x - 9,
-                            x => x,
-                        }
-                    })
-                    .collect::<Vec<_>>()
-            })
-            .collect();
-
-        Self::get_cost(&actual_map, &(*width * 5), &(*height * 5))
+        utils::a_star::solve(
+            &(0, 0),
+            &(proj_width - 1, proj_height - 1),
+            &(proj_width, proj_height),
+            |_, (x, y)| {
+                Some(
+                    match map[y % height][x % width] + (x / width + y / height) as i32 {
+                        x if x > 9 => x - 9,
+                        x => x,
+                    },
+                )
+            },
+            Self::manhattan_dist,
+        )
+        .and_then(|(_, total_cost)| Some(total_cost))
     }
 
     fn parse(input: &str) -> Result<Self::Input, &str> {
@@ -53,99 +60,10 @@ impl Solution<usize, usize> for Day15 {
 }
 
 impl Day15 {
-    fn get_cost(map: &Vec<Vec<i32>>, width: &usize, height: &usize) -> Option<usize> {
-        let solver = AStar::new(map.to_owned(), *width, *height);
-        let start = (0, 0);
-        let end = (*width - 1, *height - 1);
-
-        solver
-            .solve(&start, &end, Self::manhattan_dist)
-            .and_then(|solve| Some(solver.get_path(&solve, &start, &end)))
-            .and_then(|path| Some(path.iter().map(|&(_, cost)| cost as usize).sum()))
-    }
-
-    fn manhattan_dist(a: &(usize, usize), b: &(usize, usize)) -> i32 {
+    pub fn manhattan_dist(a: &(usize, usize), b: &(usize, usize)) -> i32 {
         let (ax, ay) = *a;
         let (bx, by) = *b;
 
         (ax as i32 - bx as i32).abs() + (ay as i32 - by as i32).abs()
-    }
-}
-
-#[derive(Debug)]
-struct AStar {
-    map: HashMap<(usize, usize), i32>,
-    width: usize,
-    height: usize,
-}
-
-impl AStar {
-    pub fn new(map: Vec<Vec<i32>>, width: usize, height: usize) -> Self {
-        Self {
-            map: (0..height)
-                .cartesian_product(0..width)
-                .map(|(y, x)| ((x, y), map[y][x]))
-                .collect(),
-            width,
-            height,
-        }
-    }
-
-    pub fn get_path(
-        &self,
-        solve: &HashMap<(usize, usize), Option<(usize, usize)>>,
-        start: &(usize, usize),
-        destination: &(usize, usize),
-    ) -> Vec<((usize, usize), i32)> {
-        let mut path = vec![];
-        let mut current = destination.clone();
-
-        while current != *start {
-            path.push((current, *self.map.get(&current).unwrap()));
-
-            current = solve.get(&current).unwrap().unwrap();
-        }
-
-        path.into_iter().rev().collect()
-    }
-
-    pub fn solve(
-        &self,
-        start: &(usize, usize),
-        destination: &(usize, usize),
-        h: impl Fn(&(usize, usize), &(usize, usize)) -> i32,
-    ) -> Option<HashMap<(usize, usize), Option<(usize, usize)>>> {
-        let mut frontier = PriorityQueue::new();
-        let mut came_from: HashMap<(usize, usize), Option<(usize, usize)>> = HashMap::new();
-        let mut cost_so_far: HashMap<(usize, usize), i32> = HashMap::new();
-
-        frontier.push(*start, i32::MAX);
-        came_from.insert(*start, None);
-        cost_so_far.insert(*start, 0);
-
-        while !frontier.is_empty() {
-            let (current, _) = frontier.pop().unwrap();
-
-            if current == *destination {
-                return Some(came_from);
-            }
-
-            neighbors(self.width, self.height, current.0, current.1)
-                .iter()
-                .for_each(|&next| {
-                    let new_cost =
-                        cost_so_far.get(&current).unwrap_or(&0) + self.map.get(&next).unwrap_or(&0);
-
-                    if new_cost < *cost_so_far.get(&next).unwrap_or(&i32::MAX) {
-                        let priority = new_cost + h(&destination, &next);
-
-                        cost_so_far.insert(next, new_cost);
-                        frontier.push(next, i32::MAX - priority);
-                        came_from.insert(next, Some(current));
-                    }
-                })
-        }
-
-        None
     }
 }
--- 2021/src/utils.rs
+++ 2021/src/utils.rs
@@ -9,3 +9,65 @@ pub fn neighbors(width: usize, height: usize, x: usize, y: usize) -> Vec<(usize,
     .filter_map(|&c| c)
     .collect()
 }
+
+pub mod a_star {
+    use crate::utils::neighbors;
+    use std::collections::{BinaryHeap, HashMap};
+    type Coord = (usize, usize);
+    type Boundaries = (usize, usize);
+
+    pub fn solve(
+        start: &Coord,
+        target: &Coord,
+        boundaries: &Boundaries,
+        get_cost: impl Fn(&Coord, &Coord) -> Option<i32>,
+        heuristic: impl Fn(&Coord, &Coord) -> i32,
+    ) -> Option<(Vec<Coord>, i32)> {
+        let mut frontier = BinaryHeap::new();
+        let mut came_from: HashMap<Coord, Option<Coord>> = HashMap::new();
+        let mut cost_so_far: HashMap<Coord, i32> = HashMap::new();
+
+        frontier.push((i32::MAX, *start));
+        came_from.insert(*start, None);
+        cost_so_far.insert(*start, 0);
+
+        while let Some((cost, current)) = frontier.pop() {
+            if current == *target {
+                return Some((get_path(&came_from, start, target), i32::MAX - cost));
+            }
+
+            neighbors(boundaries.0, boundaries.1, current.0, current.1)
+                .iter()
+                .for_each(|&next| {
+                    let new_cost = cost_so_far.get(&current).unwrap_or(&0)
+                        + get_cost(&current, &next).unwrap_or(0);
+
+                    if new_cost < *cost_so_far.get(&next).unwrap_or(&i32::MAX) {
+                        let priority = new_cost + heuristic(&target, &next);
+
+                        cost_so_far.insert(next, new_cost);
+                        frontier.push((i32::MAX - priority, next));
+                        came_from.insert(next, Some(current));
+                    }
+                })
+        }
+
+        None
+    }
+    fn get_path(
+        solve: &HashMap<(usize, usize), Option<(usize, usize)>>,
+        start: &(usize, usize),
+        destination: &(usize, usize),
+    ) -> Vec<(usize, usize)> {
+        let mut path = vec![];
+        let mut current = destination.clone();
+
+        while current != *start {
+            path.push(current);
+
+            current = solve.get(&current).unwrap().unwrap();
+        }
+
+        path.into_iter().rev().collect()
+    }
+}
