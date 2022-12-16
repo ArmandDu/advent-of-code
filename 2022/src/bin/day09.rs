f8bbbb231b0ed0b389658394eed65dc80edde7f5
Armand Dupuis <armand.dupuis@newthings.co>
2022-12-16 12:12:49 +0200

    Write Day09 2022 more rusty like

--- 2022/src/bin/day09.rs
+++ 2022/src/bin/day09.rs
@@ -1,34 +1,58 @@
+use aoc::solution::SolutionError;
 use aoc::Solution;
 use std::collections::HashSet;
 
 struct Day09;
 
-impl Day09 {
-    fn move_head(head: (i32, i32), delta: &(i32, i32)) -> (i32, i32) {
-        (head.0 + delta.0, head.1 + delta.1)
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, Default)]
+struct Knot(i32, i32);
+struct Direction(i32, i32);
+struct Instruction(Direction, i32);
+
+impl TryFrom<&str> for Instruction {
+    type Error = SolutionError;
+
+    fn try_from(line: &str) -> Result<Self, Self::Error> {
+        let (dir, count) = line.split_once(' ').ok_or(SolutionError::ParseError)?;
+
+        let count = count.parse().or(Err(SolutionError::ParseError))?;
+        let dir = match dir {
+            "R" => Direction(1, 0),
+            "L" => Direction(-1, 0),
+            "U" => Direction(0, -1),
+            "D" => Direction(0, 1),
+            _ => return Err(SolutionError::ParseError),
+        };
+
+        Ok(Instruction(dir, count))
+    }
+}
+
+impl Knot {
+    fn move_head(self, delta: &Direction) -> Self {
+        Self(self.0 + delta.0, self.1 + delta.1)
     }
 
-    fn move_tail(from: (i32, i32), to: &(i32, i32)) -> (i32, i32) {
-        let (delta_x, delta_y) = (to.0 - from.0, to.1 - from.1);
+    fn move_body(self, to: &Self) -> Self {
+        let (delta_x, delta_y) = (to.0 - self.0, to.1 - self.1);
 
         match (delta_x.abs(), delta_y.abs()) {
-            (0, 1) | (1, 1) | (1, 0) | (0, 0) => from,
-            (x, y) if x <= 2 && y <= 2 => (from.0 + delta_x.signum(), from.1 + delta_y.signum()),
-            (0, 2) => (from.0 + delta_x.signum(), from.1 + delta_y),
-            (2, 0) => (from.0 + delta_x, from.1 + delta_y.signum()),
-            _ => unreachable!(),
+            (0, 1) | (1, 1) | (1, 0) | (0, 0) => self,
+            _ => Self(self.0 + delta_x.signum(), self.1 + delta_y.signum()),
         }
     }
+}
 
-    fn move_rope(instructions: &[((i32, i32), i32)], rope_size: usize) -> HashSet<(i32, i32)> {
+impl Day09 {
+    fn move_rope<const SIZE: usize>(instructions: &[Instruction]) -> HashSet<Knot> {
         let (_, history) = instructions.iter().fold(
-            (vec![(0, 0); rope_size], HashSet::new()),
-            |(mut body, mut history), (delta, count)| {
+            ([Knot::default(); SIZE], HashSet::new()),
+            |(mut body, mut history), Instruction(delta, count)| {
                 for _ in 1..=*count {
-                    for index in 0..rope_size {
+                    for index in 0..SIZE {
                         body[index] = match index {
-                            0 => Day09::move_head(body[0], delta),
-                            _ => Day09::move_tail(body[index], &body[index - 1]),
+                            0 => body[0].move_head(delta),
+                            _ => body[index].move_body(&body[index - 1]),
                         };
                     }
 
@@ -46,35 +70,23 @@ impl Day09 {
 impl Solution for Day09 {
     const TITLE: &'static str = "Rope Bridge";
     const DAY: u8 = 9;
-    type Input = Vec<((i32, i32), i32)>;
+    type Input = Vec<Instruction>;
     type P1 = usize;
     type P2 = usize;
 
     fn parse(input: &str) -> aoc::solution::Result<Self::Input> {
         Ok(input
             .lines()
-            .filter_map(|line| {
-                let (dir, count) = line.split_once(' ')?;
-
-                let dir = match dir {
-                    "R" => Some((1, 0)),
-                    "L" => Some((-1, 0)),
-                    "U" => Some((0, -1)),
-                    "D" => Some((0, 1)),
-                    _ => None,
-                };
-
-                Some((dir?, count.parse().ok()?))
-            })
+            .filter_map(|line| Instruction::try_from(line).ok())
             .collect())
     }
 
     fn part1(input: &Self::Input) -> Option<Self::P1> {
-        Some(Day09::move_rope(input, 2).len())
+        Some(Day09::move_rope::<2>(input).len())
     }
 
     fn part2(input: &Self::Input) -> Option<Self::P2> {
-        Some(Day09::move_rope(input, 10).len())
+        Some(Day09::move_rope::<10>(input).len())
     }
 }
 
