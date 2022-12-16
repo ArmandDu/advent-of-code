702c3403262866de43231a76058976f834a356d6
Armand Dupuis <armand.dupuis@newthings.co>
2022-12-17 00:19:35 +0200

    End of Day 14 2022

--- 2022/src/bin/day14.rs
+++ 2022/src/bin/day14.rs
@@ -1,17 +1,17 @@
 use aoc::solution::SolutionError;
 use aoc::Solution;
 use itertools::{Itertools, Product};
+use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 
 use aoc_utils::dijkstra::Boundaries;
 use itertools::FoldWhile::{Continue, Done};
-use std::iter::{FlatMap, Map};
+use std::iter::Map;
 use std::ops::RangeInclusive;
-use std::slice::Iter;
 
 struct Day14;
 
-#[derive(Debug, Copy, Clone, Default)]
+#[derive(Debug, Copy, Clone, Default, Eq, PartialEq, Hash)]
 struct Point(usize, usize);
 
 impl TryFrom<&str> for Point {
@@ -62,25 +62,37 @@ enum Tile {
     Rock,
 }
 
+#[derive(Debug, Copy, Clone)]
+enum Mode {
+    Bottomless,
+    Bottom(usize),
+}
+
 #[derive(Debug, Clone)]
 struct Grid {
     boundaries: Boundaries,
-    tiles: Vec<Vec<Tile>>,
+    mode: Mode,
+    tiles: HashMap<Point, Tile>,
 }
 
 impl Display for Grid {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let depth = match self.mode {
+            Mode::Bottomless => 0,
+            Mode::Bottom(depth) => depth,
+        };
+
         write!(
             f,
             "{}",
-            self.tiles
-                .iter()
-                .map(|row| {
-                    row.iter()
-                        .map(|tile| match tile {
-                            Tile::Empty => ".",
-                            Tile::Sand => "o",
-                            Tile::Rock => "#",
+            (self.boundaries.y..=self.boundaries.height + depth)
+                .map(|j| {
+                    (self.boundaries.x - depth..=self.boundaries.width + depth)
+                        .map(|i| match self.get(&Point(i, j)) {
+                            Some(Tile::Empty) => ".",
+                            Some(Tile::Sand) => "o",
+                            Some(Tile::Rock) => "#",
+                            None => "X",
                         })
                         .collect::<String>()
                 })
@@ -113,52 +125,58 @@ impl TryFrom<&Vec<Line>> for Grid {
             height,
         };
 
-        let grid = Self {
-            boundaries: boundaries.clone(),
-            tiles: (boundaries.y..=boundaries.height)
-                .map(|_| {
-                    (boundaries.x..=boundaries.width)
-                        .map(|_| Tile::Empty)
-                        .collect()
-                })
+        Ok(Self {
+            boundaries,
+            mode: Mode::Bottomless,
+            tiles: lines
+                .iter()
+                .flat_map(|line| line.iter())
+                .map(|point| (point, Tile::Rock))
                 .collect(),
-        };
-
-        let grid = lines
-            .iter()
-            .flat_map(|line| line.iter())
-            .fold(grid, |mut grid, point| {
-                if let Some(tile) = grid.get_mut(&point) {
-                    *tile = Tile::Rock
-                };
-
-                grid
-            });
-
-        Ok(grid)
+        })
     }
 }
 
-type GridIter<'a> =
-    FlatMap<Iter<'a, Vec<Tile>>, Iter<'a, Tile>, fn(&'a Vec<Tile>) -> Iter<'_, Tile>>;
-
 impl Grid {
-    fn iter(&self) -> GridIter {
-        self.tiles.iter().flat_map(|row| row.iter())
+    fn is_inbounds(&self, point: &Point) -> bool {
+        point.1 >= self.boundaries.y && point.1 <= self.boundaries.height
     }
 
     fn get_mut(&mut self, point: &Point) -> Option<&mut Tile> {
-        let x = point.0.checked_sub(self.boundaries.x)?;
-        let y = point.1.checked_sub(self.boundaries.y)?;
-
-        self.tiles.get_mut(y)?.get_mut(x)
+        match self.mode {
+            Mode::Bottomless => self
+                .is_inbounds(point)
+                .then(|| self.tiles.entry(*point).or_insert(Tile::Empty)),
+            Mode::Bottom(depth) => {
+                if point.1 == self.boundaries.height + depth {
+                    Some(self.tiles.entry(*point).or_insert(Tile::Rock))
+                } else if point.1 >= self.boundaries.y && point.1 <= self.boundaries.height + depth
+                {
+                    Some(self.tiles.entry(*point).or_insert(Tile::Empty))
+                } else {
+                    None
+                }
+            }
+        }
     }
 
     fn get(&self, point: &Point) -> Option<&Tile> {
-        let x = point.0.checked_sub(self.boundaries.x)?;
-        let y = point.1.checked_sub(self.boundaries.y)?;
-
-        self.tiles.get(y)?.get(x)
+        match self.mode {
+            Mode::Bottomless => self
+                .is_inbounds(point)
+                .then(|| self.tiles.get(point).or(Some(&Tile::Empty)))
+                .flatten(),
+            Mode::Bottom(depth) => {
+                if point.1 == self.boundaries.height + depth {
+                    self.tiles.get(point).or(Some(&Tile::Rock))
+                } else if point.1 >= self.boundaries.y && point.1 <= self.boundaries.height + depth
+                {
+                    self.tiles.get(point).or(Some(&Tile::Empty))
+                } else {
+                    None
+                }
+            }
+        }
     }
 }
 
@@ -169,10 +187,6 @@ impl Day14 {
                 let down = Point(sand.0, sand.1 + 1);
                 let left = Point(sand.0 - 1, sand.1 + 1);
                 let right = Point(sand.0 + 1, sand.1 + 1);
-
-                // print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-                // println!("{}", grid);
-                // sleep(Duration::from_millis(1000 / 30));
 
                 if let Some(Tile::Sand) = grid.get(&leak) {
                     return Done((grid, sand));
@@ -217,16 +231,21 @@ impl Solution for Day14 {
     }
 
     fn part1(input: &Self::Input) -> Option<Self::P1> {
-        Some(
-            Day14::simulate(input.try_into().ok()?, Point(500, 0))
-                .iter()
-                .filter(|tile| matches!(tile, Tile::Sand))
-                .count(),
-        )
+        let grid = Grid::try_from(input).ok()?;
+        let rock_count = grid.tiles.len();
+
+        Some(Day14::simulate(grid, Point(500, 0)).tiles.len() - rock_count)
     }
 
-    fn part2(_input: &Self::Input) -> Option<Self::P2> {
-        None
+    fn part2(input: &Self::Input) -> Option<Self::P2> {
+        let mut grid = Grid::try_from(input).ok()?;
+        let rock_count = grid.tiles.len();
+
+        grid.mode = Mode::Bottom(2);
+
+        let grid = Day14::simulate(grid, Point(500, 0));
+
+        Some(grid.tiles.len() - rock_count)
     }
 }
 
@@ -245,6 +264,6 @@ mod tests {
      [case_1]
         - "498,4 -> 498,6 -> 496,6\n503,4 -> 502,4 -> 502,9 -> 494,9\n"
             => Some(24)
-            => None;
+            => Some(93);
     }
 }
