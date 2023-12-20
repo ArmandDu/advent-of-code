18105eeddb9c182efd090146432418fe7106e2e5
ArmandDu <mail@armanddu.com>
2023-12-20 23:47:47 +0200

    end of day 18 2023

--- 2023/src/bin/day18.rs
+++ 2023/src/bin/day18.rs
@@ -1,146 +1,195 @@
+use std::str::FromStr;
+
 use aoc::solution::SolutionError;
 use aoc::Solution;
 use itertools::Itertools;
-use std::collections::{HashSet, VecDeque};
-use std::env::args;
-use std::ops::Range;
-use std::str::FromStr;
 
-fn print() -> bool {
-    args().any(|c| c.contains("--print"))
+use crate::canvas::{Line, Point};
+use crate::viz::print;
+
+mod viz {
+    use std::env::args;
+    use std::ops::RangeInclusive;
+
+    use itertools::Itertools;
+
+    use crate::canvas::{Line, Point};
+    use crate::DigSite;
+
+    pub fn print() -> bool {
+        args().any(|c| c.contains("--print"))
+    }
+
+    impl Line {
+        fn contains(&self, point: &Point) -> bool {
+            self.intersects_x(point.x()) && self.intersects_y(point.y())
+        }
+
+        fn intersects_x(&self, x: isize) -> bool {
+            let x0 = self.start().x().min(self.end().x());
+            let x1 = self.start().x().max(self.end().x());
+
+            x0 <= x && x <= x1
+        }
+        fn intersects_y(&self, y: isize) -> bool {
+            let y0 = self.start().y().min(self.end().y());
+            let y1 = self.start().y().max(self.end().y());
+
+            y0 <= y && y <= y1
+        }
+    }
+
+    impl DigSite {
+        fn range_y(&self) -> RangeInclusive<isize> {
+            let (start, end) = self
+                .0
+                .iter()
+                .flat_map(|line| [line.start().y(), line.end().y()])
+                .minmax()
+                .into_option()
+                .map(|(y0, y1)| (y0, y1))
+                .unwrap_or_default();
+
+            start..=end
+        }
+        fn range_x(&self) -> RangeInclusive<isize> {
+            let (start, end) = self
+                .0
+                .iter()
+                .flat_map(|line| [line.start().x(), line.end().x()])
+                .minmax()
+                .into_option()
+                .map(|(x0, x1)| (x0, x1))
+                .unwrap_or_default();
+
+            start..=end
+        }
+        pub fn print(&self) {
+            for y in self.range_y() {
+                for x in self.range_x() {
+                    match self.0.iter().any(|line| line.contains(&Point::new(x, y))) {
+                        true => print!("█"),
+                        _ => print!(" "),
+                    }
+                }
+                println!()
+            }
+            println!();
+        }
+    }
+}
+
+mod canvas {
+    #[derive(Debug, Default, Hash, Eq, PartialEq, Copy, Clone, Ord, PartialOrd)]
+    pub struct Point(isize, isize);
+
+    #[derive(Default, Debug, Hash, Eq, PartialEq, Copy, Clone)]
+    pub struct Line(Point, Point);
+
+    impl Point {
+        pub fn new(x: isize, y: isize) -> Self {
+            Self(x, y)
+        }
+        pub fn x(&self) -> isize {
+            self.0
+        }
+
+        pub fn y(&self) -> isize {
+            self.1
+        }
+    }
+
+    impl Line {
+        pub fn new(start: Point, end: Point) -> Self {
+            Self(start, end)
+        }
+        pub fn dist(&self) -> usize {
+            let Line(Point(x0, y0), Point(x1, y1)) = self;
+
+            (x1.abs_diff(*x0)) + (y1.abs_diff(*y0))
+        }
+
+        pub fn start(&self) -> &Point {
+            &self.0
+        }
+        pub fn end(&self) -> &Point {
+            &self.1
+        }
+    }
 }
 
 struct Day18;
 
 #[derive(Debug)]
-struct DigInstruction(String, usize, String);
-
-impl DigInstruction {
-    fn part1(&self) -> (String, usize) {
-        (self.0.to_owned(), self.1)
-    }
-}
+struct Instruction(String, isize, String);
 
 #[derive(Debug, Default)]
-struct DigSite {
-    borders: HashSet<(isize, isize)>,
-    range_x: (isize, isize),
-    range_y: (isize, isize),
-}
+struct DigSite(Vec<Line>);
 
-impl DigSite {
-    fn new(instructions: &[(String, usize)]) -> Self {
-        let borders = Self::dig(instructions);
-
-        Self {
-            range_x: borders
-                .iter()
-                .map(|(x, _)| *x)
-                .minmax()
-                .into_option()
-                .map(|(x0, x1)| (x0 - 1, x1 + 1))
-                .unwrap_or_default(),
-            range_y: borders
-                .iter()
-                .map(|(_, y)| *y)
-                .minmax()
-                .into_option()
-                .map(|(y0, y1)| (y0 - 1, y1 + 1))
-                .unwrap_or_default(),
-            borders,
-        }
-    }
-
-    fn range_y(&self) -> Range<isize> {
-        let (start, end) = self.range_y;
-
-        start..end
-    }
-    fn range_x(&self) -> Range<isize> {
-        let (start, end) = self.range_x;
-
-        start..end
-    }
-
-    fn dig(instructions: &[(String, usize)]) -> HashSet<(isize, isize)> {
-        let mut area = HashSet::new();
-        let mut pos = (0_isize, 0_isize);
-
-        for (dir, size) in instructions {
-            let (dx, dy) = match &dir[..] {
+impl Instruction {
+    fn by_values(&self) -> ((isize, isize), isize) {
+        (
+            match &self.0[..] {
                 "D" => (0, 1),
                 "U" => (0, -1),
                 "L" => (-1, 0),
                 "R" => (1, 0),
                 _ => unreachable!(),
-            };
-
-            for _ in 0..*size {
-                pos = (pos.0 + dx, pos.1 + dy);
-                area.insert(pos);
-            }
-        }
-        area
+            },
+            self.1,
+        )
     }
 
-    fn border_size(&self) -> usize {
-        self.borders.len()
-    }
+    fn by_color(&self) -> Option<((isize, isize), isize)> {
+        let size = isize::from_str_radix(&self.2[1..6], 16).ok()?;
 
-    fn pool(&self) -> Option<HashSet<(isize, isize)>> {
-        let start = self.range_y().find_map(|y| {
-            let x = self
-                .borders
-                .iter()
-                .filter_map(|(kx, ky)| (ky == &y).then_some(kx))
-                .min()?
-                + 1;
+        let dir = match &self.2[6..] {
+            "0" => (1, 0),
+            "1" => (0, 1),
+            "2" => (-1, 0),
+            "3" => (0, -1),
+            _ => unreachable!(),
+        };
 
-            (!self.borders.contains(&(x, y))).then_some((x, y))
-        })?;
-
-        let mut history = HashSet::new();
-        let mut queue = VecDeque::new();
-
-        queue.push_front(start);
-
-        while let Some(current) = queue.pop_front() {
-            if self.borders.contains(&current) {
-                continue;
-            }
-
-            if !history.insert(current) {
-                continue;
-            }
-
-            for (dx, dy) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
-                let next = (current.0 + dx, current.1 + dy);
-
-                queue.push_front(next);
-            }
-        }
-
-        Some(history)
-    }
-
-    fn print(&self, pool: &HashSet<(isize, isize)>) {
-        for y in self.range_y() {
-            for x in self.range_x() {
-                match pool.contains(&(x, y)) {
-                    true => print!("o"),
-                    _ => match self.borders.get(&(x, y)) {
-                        Some(_) => print!("#"),
-                        _ => print!("."),
-                    },
-                }
-            }
-            println!()
-        }
-        println!();
+        Some((dir.to_owned(), size))
     }
 }
 
-impl FromStr for DigInstruction {
+impl DigSite {
+    fn new(instructions: &[((isize, isize), isize)]) -> Self {
+        Self(
+            instructions
+                .iter()
+                .fold(((0, 0), vec![]), |(start, mut lines), ((dx, dy), size)| {
+                    let end = (start.0 + dx * size, start.1 + dy * size);
+
+                    lines.push(Line::new(
+                        Point::new(start.0, start.1),
+                        Point::new(end.0, end.1),
+                    ));
+
+                    (end, lines)
+                })
+                .1,
+        )
+    }
+
+    fn border_size(&self) -> usize {
+        self.0.iter().map(|line| line.dist()).sum()
+    }
+
+    fn area(&self) -> Option<usize> {
+        self.0
+            .iter()
+            .map(|line| line.start().x() * line.end().y() - line.end().x() * line.start().y())
+            .sum1::<isize>()
+            .map(|area| area.unsigned_abs())
+            .map(|area| area + self.border_size())
+            .map(|area| area / 2 + 1)
+    }
+}
+
+impl FromStr for Instruction {
     type Err = SolutionError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -150,7 +199,7 @@ impl FromStr for DigInstruction {
             .ok_or(SolutionError::ParseError)?;
         let size = size.parse().map_err(|_| SolutionError::ParseError)?;
 
-        Ok(DigInstruction(
+        Ok(Instruction(
             dir.to_owned(),
             size,
             color.replace(['(', ')'], ""),
@@ -161,28 +210,26 @@ impl FromStr for DigInstruction {
 impl Solution for Day18 {
     const TITLE: &'static str = "Lavaduct Lagoon";
     const DAY: u8 = 18;
-    type Input = Vec<DigInstruction>;
+    type Input = Vec<Instruction>;
     type P1 = usize;
     type P2 = usize;
 
     fn parse(input: &str) -> aoc::solution::Result<Self::Input> {
-        input.lines().map(DigInstruction::from_str).collect()
+        input.lines().map(Instruction::from_str).collect()
     }
 
     fn part1(input: &Self::Input) -> Option<Self::P1> {
-        let dig_site = DigSite::new(&input.iter().map(|inst| inst.part1()).collect_vec());
-        let pool = dig_site.pool()?;
-        let size = dig_site.border_size();
+        let dig_site = DigSite::new(&input.iter().map(Instruction::by_values).collect_vec());
 
         if print() {
-            dig_site.print(&pool);
+            dig_site.print();
         }
 
-        Some(pool.len() + size)
+        dig_site.area()
     }
 
-    fn part2(_input: &Self::Input) -> Option<Self::P2> {
-        None
+    fn part2(input: &Self::Input) -> Option<Self::P2> {
+        DigSite::new(&input.iter().filter_map(Instruction::by_color).collect_vec()).area()
     }
 }
 
@@ -192,5 +239,5 @@ aoc::example! {
     [Day18]
     sample: "R 6 (#70c710)\r\nD 5 (#0dc571)\r\nL 2 (#5713f0)\r\nD 2 (#d2c081)\r\nR 2 (#59c680)\r\nD 2 (#411b91)\r\nL 5 (#8ceee2)\r\nU 2 (#caa173)\r\nL 1 (#1b58a2)\r\nU 2 (#caa171)\r\nR 2 (#7807d2)\r\nU 3 (#a77fa3)\r\nL 2 (#015232)\r\nU 2 (#7a21e3)\r\n"
         => Some(62)
-        => None
+        => Some(952408144115)
 }
