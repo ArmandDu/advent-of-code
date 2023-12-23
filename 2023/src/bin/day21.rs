960e244672f1f890d17a24ef79a3e6d701a84479
ArmandDu <mail@armanddu.com>
2023-12-24 00:13:59 +0200

    end of day 21 part 2 2023

--- 2023/src/bin/day21.rs
+++ 2023/src/bin/day21.rs
@@ -3,6 +3,7 @@ use std::str::FromStr;
 
 use aoc::solution::SolutionError;
 use aoc::Solution;
+use itertools::Itertools;
 
 use aoc_utils::is_flag;
 
@@ -38,20 +39,19 @@ impl Garden {
 }
 
 impl Garden {
-    fn solve(&self, max_steps: usize) -> Option<HashSet<(usize, usize)>> {
-        let start = self.start()?;
+    fn solve(&self, start: (usize, usize), max_steps: usize) -> HashSet<(usize, usize)> {
         let mut visited = HashSet::from([start]);
         let mut accepted = HashSet::new();
 
         let mut queue = VecDeque::from([(start, max_steps)]);
 
         while let Some((current, steps)) = queue.pop_front() {
-            if steps % 2 == max_steps % 2 {
+            if steps % 2 == 0 {
                 accepted.insert(current);
             }
 
             if steps > 0 {
-                for next in self.adjacent(&current)? {
+                for next in self.adjacent(&current) {
                     if visited.insert(next) {
                         queue.push_back((next, steps - 1));
                     }
@@ -59,7 +59,7 @@ impl Garden {
             }
         }
 
-        Some(accepted)
+        accepted
     }
 
     fn start(&self) -> Option<(usize, usize)> {
@@ -67,16 +67,25 @@ impl Garden {
             .map(|(coord, _)| coord.to_owned())
     }
 
-    fn adjacent(&self, node: &(usize, usize)) -> Option<Vec<(usize, usize)>> {
+    fn adjacent(&self, node: &(usize, usize)) -> Vec<(usize, usize)> {
         let (x, y) = node;
 
-        Some(
-            [(0, 1), (0, -1), (1, 0), (-1, 0)]
-                .into_iter()
-                .filter_map(|(dx, dy)| Some((x.checked_add_signed(dx)?, y.checked_add_signed(dy)?)))
-                .filter(|coord| self.get(coord) != Some(&'#'))
-                .collect(),
-        )
+        [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            .into_iter()
+            .filter_map(|(dx, dy)| Some((x.checked_add_signed(dx)?, y.checked_add_signed(dy)?)))
+            .filter(|(x, y)| *x < self.width() && *y < self.height())
+            .filter(|coord| self.get(coord) != Some(&'#'))
+            .collect()
+    }
+}
+
+impl Garden {
+    fn width(&self) -> usize {
+        self.0.first().map(|row| row.len()).unwrap_or_default()
+    }
+
+    fn height(&self) -> usize {
+        self.0.len()
     }
 }
 
@@ -92,7 +101,7 @@ impl<const STEPS: usize> Solution for Day21<STEPS> {
     }
 
     fn part1(input: &Self::Input) -> Option<Self::P1> {
-        let accepted = input.solve(STEPS)?;
+        let accepted = input.solve(input.start()?, STEPS);
 
         if is_flag("--print") {
             viz::print(&input.0, &accepted);
@@ -101,8 +110,66 @@ impl<const STEPS: usize> Solution for Day21<STEPS> {
         Some(accepted.len())
     }
 
-    fn part2(_input: &Self::Input) -> Option<Self::P2> {
-        None
+    fn part2(input: &Self::Input) -> Option<Self::P2> {
+        const STEPS: usize = 26501365;
+
+        let grid_size = (input.width() == input.height()).then_some(input.height())?;
+        let checkerboard_width = STEPS / grid_size;
+
+        if STEPS % grid_size != grid_size / 2 {
+            return None;
+        }
+
+        let n_odd_boxes = (checkerboard_width - 1).pow(2);
+        let n_even_boxes = checkerboard_width.pow(2);
+
+        let start = input.start()?;
+        let (start_x, start_y) = start;
+        let cardinal_edges = [
+            (0, start_y),
+            (grid_size - 1, start_y),
+            (start_x, 0),
+            (start_x, grid_size - 1),
+        ];
+
+        let diagonal_edges = [
+            (0, grid_size - 1),
+            (grid_size - 1, grid_size - 1),
+            (0, 0),
+            (grid_size - 1, 0),
+        ];
+
+        //(start[], steps, n_boxes)
+        //solve(start, steps) * n_boxes
+        [
+            //odd boxes
+            (vec![start], grid_size + grid_size % 2 + 1, n_odd_boxes),
+            //even boxes
+            (vec![start], grid_size + grid_size % 2, n_even_boxes),
+            //cardinal edges
+            (cardinal_edges.to_vec(), grid_size - 1, 1),
+            //even edges
+            (
+                diagonal_edges.to_vec(),
+                grid_size / 2 - 1,
+                checkerboard_width,
+            ),
+            //odd edges
+            (
+                diagonal_edges.to_vec(),
+                grid_size * 3 / 2 - 1,
+                checkerboard_width - 1,
+            ),
+        ]
+        .into_iter()
+        .map(|(edges, steps, n_boxes)| {
+            edges
+                .into_iter()
+                .map(|start| input.solve(start, steps).len())
+                .sum::<usize>()
+                * n_boxes
+        })
+        .sum1()
     }
 }
 
@@ -116,6 +183,7 @@ mod viz {
                     true => print!("O"),
                     _ => print!("{tile}"),
                 }
+                print!(" ")
             }
             println!()
         }
